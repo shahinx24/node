f219567ce4d516fc150b21255045e02abb0dc77a
shahinx24 <shahinofficial911@gmail.com>
2026-05-30 15:09:35 +0530

    update

--- fs.js
+++ fs.js
@@ -1,10 +1,10 @@
 // create
-const fs = require("fs");
+// const fs = require("fs");
 
-fs.writeFile("demo.txt", "Hello Shahin!", (err) => {
-    if (err) throw err;
-    console.log("File created");
-});
+// fs.writeFile("demo.txt", "Hello Shahin!", (err) => {
+//     if (err) throw err;
+//     console.log("File created");
+// });
 
 // read
 // const fs = require("fs");
@@ -14,3 +14,21 @@ fs.writeFile("demo.txt", "Hello Shahin!", (err) => {
 //     console.log(data);
 // });
 
+// adding without delete old one
+// const fs = require("fs");
+// fs.appendFile("demo.txt", "\nWelcome to Node.js", (err) => {
+//     if (err) throw err;
+//     console.log("Data added");
+// });
+
+// Rename
+fs.rename("demo.txt", "sample.txt", (err) => {
+    if (err) throw err;
+    console.log("File renamed");
+});
+
+// Delete
+// fs.unlink("sample.txt", (err) => {
+//     if (err) throw err;
+//     console.log("File deleted");
+// });
