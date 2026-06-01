64679459d57bc4de81b4b1dd4021b66dd01ec874
shahinx24 <shahinofficial911@gmail.com>
2026-06-01 10:30:40 +0530

    update

--- buffer.js
+++ buffer.js
@@ -1,26 +1,26 @@
 // Buffer
-// const fs = require('fs')
+const fs = require('fs')
 
-// fs.readFile("demo.txt", (err,data)=>{
-//     if(err) throw err;
-//     console.log(data)
-// })
+fs.readFile("demo.txt", (err,data)=>{
+    if(err) throw err;
+    console.log(data)
+})
 
 
 //stream
-const fs = require('fs')
+// const fs = require('fs')
 
-const readStream = fs.createReadStream("demo.txt", "utf8");
+// const readStream = fs.createReadStream("demo.txt", "utf8");
 
-readStream.on('data', (chunk)=>{
-    console.log("chunk received")
-    console.log(chunk)
-})
+// readStream.on('data', (chunk)=>{
+//     console.log("chunk received")
+//     console.log(chunk)
+// })
 
-readStream.on('end', ()=>{
-    console.log("file read completed")
-})
+// readStream.on('end', ()=>{
+//     console.log("file read completed")
+// })
 
-readStream.on("error", (err)=>{
-    console.log("error: ", err.message);
-})
+// readStream.on("error", (err)=>{
+//     console.log("error: ", err.message);
+// })
