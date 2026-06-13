4a821e5cc4b50a34b01d18f6d9e9276e3b4920f1
shahinx24 <shahinofficial911@gmail.com>
2026-06-13 14:46:56 +0530

    update

---
+++ upload/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const multer = require('multer')
+
+const app = express();
+
+const storage = multer.diskStorage({
+    destination: (req,file,cb) =>{
+        cb(null, 'uploads/');
+    },
+    filename: (req,file, cb)=>{
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+})
+
+const upload = multer({ storage });
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.send('File uploaded successfully');
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
