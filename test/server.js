e491d542a4700894a63b9977c9675c320f0e4ed1
shahinx24 <shahinofficial911@gmail.com>
2026-06-17 10:32:08 +0530

    update

---
+++ test/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res)=>{
+        res.send("hello node")
+})
+
+app.listen(3000,()=>{
+    console.log(`Server is live on http://localhost:3000`)
+})
