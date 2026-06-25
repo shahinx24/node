8ba8289ebb411cfdec7bbb1cfc12413d38adcd5e
shahinx24 <shahinofficial911@gmail.com>
2026-06-25 10:49:41 +0530

    update

--- sample/sr.js
+++ sample/sr.js
@@ -12,3 +12,26 @@ const app = express()
         console.log(error)
     }
   }
+  connectDB
+
+  const carsSchema= new mongoose.Schema({
+    name:String,
+    brand:String,
+    model:Number
+  })
+  const cars =  mongoose.model("sedan",carsSchema)
+
+  app.post("/cars", async (req,res)=>{
+    console.log("POST rout hit")
+    try {
+        const data = await cars.create(req.body);
+        res.status(201).json(data);
+    }catch (error){
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+  });
+
+  app.listen(5000,()=>{
+    console.log("server is live on http://localhost:5000")
+  })
